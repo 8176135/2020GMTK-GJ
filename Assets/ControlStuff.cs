5d28afda50b9349942071bdb0818cc97cd95bdb6
8176135 <elsecaller@8176135.xyz>
2020-07-11 10:12:44 +1200

    Init commit

---
+++ Assets/ControlStuff.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlStuff : MonoBehaviour
+{
+	private Rigidbody2D rb;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+
+	private void FixedUpdate()
+	{
+		rb.AddForce(Vector2.up * 10);
+	}
+}
